d0a27f782789741b119f4b16ba3ea3b843ae7c92
olenaazarova <azarova.pn@ucu.edu.ua>
2024-05-16 21:00:49 +0300

    adjust search.js

--- proj-login/src/components/Search/Search.js
+++ proj-login/src/components/Search/Search.js
@@ -1,47 +1,42 @@
-import React, {useState} from 'react';
-import './Search.module.css'
-import { Link, useHistory } from 'react-router-dom';
-import { Navbar, Nav } from 'react-bootstrap';
-import styles from "./Search.module.css"
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import styles from "./Search.module.css";
 
-function SearchPage({ isAuthenticated }) {
-  const [loggedIn, setLoggedIn] = useState(false);
+function SearchPage() {
   const [userEmail, setUserEmail] = useState("");
   const history = useHistory();
 
-  const navigateProfile = () => {
-    // Implement how to navigate based on selection
+  const navigateProfile = (event) => {
+    const selectedOption = event.target.value;
+    switch (selectedOption) {
+      case "viewProfile":
+        history.push("/tell_about");
+        break;
+      case "logout":
+        setUserEmail("");
+        history.push("/");
+        break;
+      default:
+        break;
+    }
   };
-  const handleAboutPage = () => {
-    history.push("/tell_about")
-  }
-  const handleLogout = () => {
-    setUserEmail("");
-    setLoggedIn(false);
-    history.push("/");
 
-}
   return (
     <>
-      {/* <header className={styles.site_header}>
-      </header> */}
-      
-      <div>
-        <div className={styles.nav_profile}>
-          <select id={styles.profile_dropdown} onChange={navigateProfile}>
-            <option value="" disabled selected>Profile</option>
-            <option value="viewProfile" onClick={handleAboutPage}>View your profile</option>
-            <option value="logout" onClick={handleLogout}>Log out</option>
-          </select>
-        </div>
+      <div className={styles.nav_profile}>
+        <select id={styles.profile_dropdown} onChange={navigateProfile} defaultValue="">
+          <option value="" disabled>Profile</option>
+          <option value="viewProfile">View your profile</option>
+          <option value="logout">Log out</option>
+        </select>
+      </div>
 
-        <div id={styles.content_search}>
-          <h2 id="search_title">We are looking for a match</h2>
-          <div className={styles.loader}></div>
-          <button id={styles.button_stop} onClick={() => history.push('/')}>
-            Stop search
-          </button>
-        </div>
+      <div id={styles.content_search}>
+        <h2>We are looking for a match</h2>
+        <div className={styles.loader}></div>
+        <button id={styles.button_stop} onClick={() => history.push('/')}>
+          Stop search
+        </button>
       </div>
     </>
   );
