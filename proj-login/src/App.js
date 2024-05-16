68fd91905ae52aa7afcfa40e733ee2f3770e5fe0
Anna <annamonastyrska.05@gmail.com>
2024-05-16 17:29:46 +0300

    add home page after log in

--- proj-login/src/App.js
+++ proj-login/src/App.js
@@ -1,65 +1,80 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
-import LoginPage from './components/LoginPage/LoginPage';
-import HomePage from './components/HomePage/HomePage';
-import Navbar from './components/Navbar/Navbar';
-import RegisterPage from './components/RegisterPage/RegisterPage';
-import GreetingPage from './components/GreetingPage/GreetingPage';
-import {auth} from './firebaseConfig';
-import {onAuthStateChanged} from 'firebase/auth';
-import './App.css';
-import TellAbout from './components/TellAbout/TellAbout';
+import React, { useState, useEffect } from "react";
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch,
+    Redirect,
+} from "react-router-dom";
+import LoginPage from "./components/LoginPage/LoginPage";
+import HomePage from "./components/HomePage/HomePage";
+import Navbar from "./components/Navbar/Navbar";
+import RegisterPage from "./components/RegisterPage/RegisterPage";
+import GreetingPage from "./components/GreetingPage/GreetingPage";
+import TellAbout from "./components/TellAbout/TellAbout";
+// import Search from "./components/Search/Search";
+import { auth } from "./firebaseConfig";
+import { onAuthStateChanged } from "firebase/auth";
+import "./App.css";
+
 
 const App = () => {
-  const [loggedIn, setLoggedIn] = useState(false);
-  const [userEmail, setUserEmail] = useState('');
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [userEmail, setUserEmail] = useState("");
 
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user) {
-        setUserEmail(user.email);
-        setLoggedIn(true);
-      } else {
-        setUserEmail('');
-        setLoggedIn(false);
-      }
-    });
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setUserEmail(user.email);
+                setLoggedIn(true);
+            } else {
+                setUserEmail("");
+                setLoggedIn(false);
+            }
+        });
 
         return () => {
             unsubscribe();
         };
     }, []);
 
-  const handleLogin = (email) => {
-    setUserEmail(email);
-    setLoggedIn(true);
-  };
+    const handleLogin = (email) => {
+        setUserEmail(email);
+        setLoggedIn(true);
+    };
 
-  const handleLogout = () => {
-    setUserEmail('');
-    setLoggedIn(false);
-  };
+    const handleLogout = () => {
+        setUserEmail("");
+        setLoggedIn(false);
+    };
 
     return (
         <Router>
             <div>
                 <Switch>
-                    <Route exact path="/" component={GreetingPage}/>
+                    <Route exact path="/" component={GreetingPage} />
                     <Route path="/login">
-                        {loggedIn ? <Redirect to="/home"/> : <LoginPage onLogin={handleLogin}/>}
-                    </Route>
-                    <Route path="/register" component={RegisterPage}/>
-                    <Route path="/home">
                         {loggedIn ? (
-                        <div>
-                          <Navbar isAuthenticated={loggedIn} onLogout={handleLogout} />
-                          <HomePage userEmail={userEmail} />
-                        </div>
+                            <Redirect to="/home" />
                         ) : (
-                          <Redirect to="/" />
+                            <LoginPage onLogin={handleLogin} />
                         )}
                     </Route>
-                    <Route path="/tell_about" component={TellAbout}/>
+                    <Route path="/register" component={RegisterPage} />
+                    <Route path="/home">
+                        {loggedIn ? (
+                            <div>
+                                <Navbar
+                                    isAuthenticated={loggedIn}
+                                    onLogout={handleLogout}
+                                />
+                                <HomePage userEmail={userEmail} />
+                            </div>
+                        ) : (
+                            <Redirect to="/" />
+                        )}
+                    </Route>
+                
+                    <Route path="/tell_about" component={TellAbout} />
                     <Route render={() => <Redirect to="/" />} />
                 </Switch>
             </div>
